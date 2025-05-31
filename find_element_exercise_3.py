a622142a2571a843dcbc370a353dcd770cdf3954
Mario Cruz <mar_cr_uval@hotmail.com>
2025-05-30 21:14:35 -0600

    Tercera iteración

--- find_element_exercise_3.py
+++ find_element_exercise_3.py
@@ -1,17 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
 driver = webdriver.Chrome()
 
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+time.sleep(5)
 
 # Buscar elementos
-email = driver...
-password = driver...
+email = driver.find_element(By.ID, "email")
+password = driver.find_element(By.ID, "password")
 
 # Probar el atributo placeholder para cada elemento
-assert ...
-assert ...
+assert email.get_attribute('placeholder') == 'Correo electrónico', f"Expected 'Correo electrónico', but got '{email.get_attribute('placeholder')}'"
+assert password.get_attribute('placeholder') == 'Contraseña', f"Expected 'Contraseña', but got '{password.get_attribute('placeholder')}'"
+print("Pruebas existosas!")
 
 # Cerrar el navegador
-driver...
+driver.quit()
--- сlick_on_an_element.py
+++ сlick_on_an_element.py
@@ -1,11 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+time.sleep(1)
 
 # Buscar el botón y hacer clic en él
-driver.find_element(...)...
+driver.find_element(By.XPATH, ".//button[@class='auth-form__button']").click()
+time.sleep(5)
 
 driver.quit()
