86b1be4b9ff26026559776d086635254df26cde8
Mario Cruz <mar_cr_uval@hotmail.com>
2025-06-04 23:03:05 -0600

    Prueba 1 completa, falta Prueba 2

--- practice_exercise_1.py
+++ practice_exercise_1.py
@@ -1,3 +1,5 @@
+import time
+import configuration
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -5,26 +7,31 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+driver.implicitly_wait(10)
 
 # Iniciar sesión
-...
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.ID, "email")))
+driver.find_element(By.ID, "email").send_keys(configuration.email)
+driver.find_element(By.ID, "password").send_keys(configuration.password)
+driver.find_element(By.CLASS_NAME, "auth-form__button").click()
 
 # Agregar una espera explícita para que se cargue la página
-...
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".profile__image")))
 
 # Hacer clic en la foto de perfil
-driver.find_element(...)...
+driver.find_element(By.CSS_SELECTOR, ".profile__image").click()
 
 # Insertar el enlace a la foto en el campo Enlace utilizando la variable avatar_url
 avatar_url = "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/qa-sprint-7/avatarSelenium.png"
-driver.find_element(...)....
+driver.find_element(By.ID, "owner-avatar").send_keys(avatar_url)
 
 # Guardar la nueva foto
-driver.find_element(...)...
+driver.find_element(By.XPATH, ".//form[@name='edit-avatar']/button[text()='Guardar']").click()
+WebDriverWait(driver, 3).until(expected_conditions.text_to_be_present_in_element_attribute((By.CSS_SELECTOR, ".profile__image"), 'style', avatar_url))
 
 # Guardar el valor del atributo de estilo para el elemento de foto de perfil en la variable style
-style = driver.find_element(...)...
+style = driver.find_element(By.CSS_SELECTOR, ".profile__image").get_attribute("style")
 # Comprobar que style contiene el enlace a la foto de perfil
-assert ...
-
+assert style == 'background-image: url("' + avatar_url + '");'
+print("Test passed")
 driver.quit()
--- practice_exercise_2.py
+++ practice_exercise_2.py
@@ -1,5 +1,6 @@
+import time
 import random
-
+import configuration
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -9,45 +10,50 @@ driver = webdriver.Chrome()
 driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
 
 # Iniciar sesión
-...
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.ID, "email")))
+driver.find_element(By.ID, "email").send_keys(configuration.email)
+driver.find_element(By.ID, "password").send_keys(configuration.password)
+driver.find_element(By.CLASS_NAME, "auth-form__button").click()
 
 # Agregar una espera explícita para que se cargue la página
-...
+driver.implicitly_wait(10)
 
 # Guardar el título de la tarjeta más reciente
-title_before = ...
-
+title_before = driver.find_element(By.XPATH, "//li[@class='places__item card']//h2[@class='card__title']").text
+print(title_before)
 # Hacer clic en el botón que publica una nueva tarjeta
-driver.find_element(...)...
+driver.find_element(By.CLASS_NAME, "profile__add-button").click()
 
 # Generar el nuevo nombre del lugar e ingresarlo en el campo Nombre
-new_title = ...
-driver.find_element(...)...
+new_title = "Tokio"+str((random.randint(100, 999)))
+driver.find_element(By.NAME, "name").send_keys(new_title)
 
 # Insertar el enlace a la imagen en el campo Enlace
-driver.find_element(...)...
+driver.find_element(By.NAME, "link").send_keys("https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/qa-sprint-7/photoSelenium.jpg")
 
 # Guardar los datos
-driver.find_element(...)...
+driver.find_element(By.XPATH, ".//form[@name='new-card']/button[text()='Guardar']").click()
 
 # Esperar a que aparezca el botón Eliminar
-WebDriverWait(...).until(...)
+WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, ".//button[@class='card__delete-button card__delete-button_visible']")))
 
 # Comprobar que la tarjeta tiene el título correcto
-title_after = ...
-assert ...
+title_after = driver.find_element(By.XPATH, "//li[@class='places__item card']//h2[@class='card__title']")
+
+assert title_after.text == new_title
 
 # Guardar la cantidad de tarjetas antes de eliminar
-cards_before = len(...)
+cards_before = len(driver.find_elements(By.XPATH, "//li[@class='places__item card']"))
 
 # Eliminar la tarjeta
-driver.find_element(...)...
+driver.find_element(By.XPATH, "//li[@class='places__item card'][1]/button[@class='card__delete-button card__delete-button_visible']").click()
 
 # Esperar a que el título de la tarjeta más reciente sea igual a title_before
-WebDriverWait(...).until(...)
+#WebDriverWait(driver, 10).until(expected_conditions.text_to_be_present_in_element( (By.XPATH, "//li[@class='places__item card']//h2[@class='card__title']"), title_before))
 
 # Comprobar que ahora hay una tarjeta menos
-cards_after = len(...)
-assert ...
+#cards_after = len(driver.find_elements(By.XPATH, "//li[@class='places__item card']"))
 
-driver.quit()
+#assert cards_before != cards_after
+
+#driver.quit()
